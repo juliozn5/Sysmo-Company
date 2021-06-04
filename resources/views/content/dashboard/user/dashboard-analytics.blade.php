0cc5520716de77b3de2edcbf972624baba6af7b1
William Ache <william28ache@gmail.com>
2021-06-04 17:26:48 -0400

    respaldo

--- resources/views/content/dashboard/user/dashboard-analytics.blade.php
+++ resources/views/content/dashboard/user/dashboard-analytics.blade.php
@@ -236,6 +236,262 @@
     </div>
     <!-- Support Tracker Chart Card ends -->
   </div>
+
+  <div class="row match-height">
+    <!-- Timeline Card -->
+    <div class="col-lg-4 col-12">
+      <div class="card card-user-timeline">
+        <div class="card-header">
+          <div class="d-flex align-items-center">
+            <i data-feather="list" class="user-timeline-title-icon"></i>
+            <h4 class="card-title">User Timeline</h4>
+          </div>
+        </div>
+        <div class="card-body">
+          <ul class="timeline ml-50 mb-0">
+            <li class="timeline-item">
+              <span class="timeline-point timeline-point-indicator"></span>
+              <div class="timeline-event">
+                <h6>12 Invoices have been paid</h6>
+                <p>Invoices are paid to the company</p>
+                <div class="media align-items-center">
+                  <img class="mr-1" src="{{asset('images/icons/json.png')}}" alt="data.json" height="23" />
+                  <h6 class="media-body mb-0">data.json</h6>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-item">
+              <span class="timeline-point timeline-point-warning timeline-point-indicator"></span>
+              <div class="timeline-event">
+                <h6>Client Meeting</h6>
+                <p>Project meeting with Carl</p>
+                <div class="media align-items-center">
+                  <div class="avatar mr-50">
+                    <img
+                      src="{{asset('images/portrait/small/avatar-s-9.jpg')}}"
+                      alt="Avatar"
+                      width="38"
+                      height="38"
+                    />
+                  </div>
+                  <div class="media-body">
+                    <h6 class="mb-0">Carl Roy (Client)</h6>
+                    <p class="mb-0">CEO of Infibeam</p>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-item">
+              <span class="timeline-point timeline-point-info timeline-point-indicator"></span>
+              <div class="timeline-event">
+                <h6>Create a new project</h6>
+                <p>Add files to new design folder</p>
+                <div class="avatar-group">
+                  <div
+                    data-toggle="tooltip"
+                    data-popup="tooltip-custom"
+                    data-placement="bottom"
+                    data-original-title="Billy Hopkins"
+                    class="avatar pull-up"
+                  >
+                    <img
+                      src="{{asset('images/portrait/small/avatar-s-9.jpg')}}"
+                      alt="Avatar"
+                      width="33"
+                      height="33"
+                    />
+                  </div>
+                  <div
+                    data-toggle="tooltip"
+                    data-popup="tooltip-custom"
+                    data-placement="bottom"
+                    data-original-title="Amy Carson"
+                    class="avatar pull-up"
+                  >
+                    <img
+                      src="{{asset('images/portrait/small/avatar-s-6.jpg')}}"
+                      alt="Avatar"
+                      width="33"
+                      height="33"
+                    />
+                  </div>
+                  <div
+                    data-toggle="tooltip"
+                    data-popup="tooltip-custom"
+                    data-placement="bottom"
+                    data-original-title="Brandon Miles"
+                    class="avatar pull-up"
+                  >
+                    <img
+                      src="{{asset('images/portrait/small/avatar-s-8.jpg')}}"
+                      alt="Avatar"
+                      width="33"
+                      height="33"
+                    />
+                  </div>
+                  <div
+                    data-toggle="tooltip"
+                    data-popup="tooltip-custom"
+                    data-placement="bottom"
+                    data-original-title="Daisy Weber"
+                    class="avatar pull-up"
+                  >
+                    <img
+                      src="{{asset('images/portrait/small/avatar-s-7.jpg')}}"
+                      alt="Avatar"
+                      width="33"
+                      height="33"
+                    />
+                  </div>
+                  <div
+                    data-toggle="tooltip"
+                    data-popup="tooltip-custom"
+                    data-placement="bottom"
+                    data-original-title="Jenny Looper"
+                    class="avatar pull-up"
+                  >
+                    <img
+                      src="{{asset('images/portrait/small/avatar-s-20.jpg')}}"
+                      alt="Avatar"
+                      width="33"
+                      height="33"
+                    />
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-item">
+              <span class="timeline-point timeline-point-danger timeline-point-indicator"></span>
+              <div class="timeline-event">
+                <h6>Update project for client</h6>
+                <p class="mb-0">Update files as per new design</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!--/ Timeline Card -->
+
+    <!-- Sales Stats Chart Card starts -->
+    <div class="col-lg-4 col-md-6 col-12">
+      <div class="card">
+        <div class="card-header d-flex justify-content-between align-items-start pb-1">
+          <div>
+            <h4 class="card-title mb-25">Sales</h4>
+            <p class="card-text">Last 6 months</p>
+          </div>
+          <div class="dropdown chart-dropdown">
+            <i data-feather="more-vertical" class="font-medium-3 cursor-pointer" data-toggle="dropdown"></i>
+            <div class="dropdown-menu dropdown-menu-right">
+              <a class="dropdown-item" href="javascript:void(0);">Last 28 Days</a>
+              <a class="dropdown-item" href="javascript:void(0);">Last Month</a>
+              <a class="dropdown-item" href="javascript:void(0);">Last Year</a>
+            </div>
+          </div>
+        </div>
+        <div class="card-body">
+          <div class="d-inline-block mr-1">
+            <div class="d-flex align-items-center">
+              <i data-feather="circle" class="font-small-3 text-primary mr-50"></i>
+              <h6 class="mb-0">Sales</h6>
+            </div>
+          </div>
+          <div class="d-inline-block">
+            <div class="d-flex align-items-center">
+              <i data-feather="circle" class="font-small-3 text-info mr-50"></i>
+              <h6 class="mb-0">Visits</h6>
+            </div>
+          </div>
+          <div id="sales-visit-chart" class="mt-50"></div>
+        </div>
+      </div>
+    </div>
+    <!-- Sales Stats Chart Card ends -->
+
+    <!-- App Design Card -->
+    <div class="col-lg-4 col-md-6 col-12">
+      <div class="card card-app-design">
+        <div class="card-body">
+          <div class="badge badge-light-primary">03 Sep, 20</div>
+          <h4 class="card-title mt-1 mb-75 pt-25">App design</h4>
+          <p class="card-text font-small-2 mb-2">
+            You can Find Only Post and Quotes Related to IOS like ipad app design, iphone app design
+          </p>
+          <div class="design-group mb-2 pt-50">
+            <h6 class="section-label">Team</h6>
+            <div class="badge badge-light-warning mr-1">Figma</div>
+            <div class="badge badge-light-primary">Wireframe</div>
+          </div>
+          <div class="design-group pt-25">
+            <h6 class="section-label">Members</h6>
+            <div class="avatar">
+              <img src="{{asset('images/portrait/small/avatar-s-9.jpg')}}" width="34" height="34" alt="Avatar" />
+            </div>
+            <div class="avatar bg-light-danger">
+              <div class="avatar-content">PI</div>
+            </div>
+            <div class="avatar">
+              <img
+                src="{{asset('images/portrait/small/avatar-s-14.jpg')}}"
+                width="34"
+                height="34"
+                alt="Avatar"
+              />
+            </div>
+            <div class="avatar">
+              <img src="{{asset('images/portrait/small/avatar-s-7.jpg')}}" width="34" height="34" alt="Avatar" />
+            </div>
+            <div class="avatar bg-light-secondary">
+              <div class="avatar-content">AL</div>
+            </div>
+          </div>
+          <div class="design-planning-wrapper mb-2 py-75">
+            <div class="design-planning">
+              <p class="card-text mb-25">Due Date</p>
+              <h6 class="mb-0">12 Apr, 21</h6>
+            </div>
+            <div class="design-planning">
+              <p class="card-text mb-25">Budget</p>
+              <h6 class="mb-0">$49251.91</h6>
+            </div>
+            <div class="design-planning">
+              <p class="card-text mb-25">Cost</p>
+              <h6 class="mb-0">$840.99</h6>
+            </div>
+          </div>
+          <button type="button" class="btn btn-primary btn-block">Join Team</button>
+        </div>
+      </div>
+    </div>
+    <!--/ App Design Card -->
+  </div>
+
+  <!-- List DataTable -->
+  <div class="row">
+    <div class="col-12">
+      <div class="card invoice-list-wrapper">
+        <div class="card-datatable table-responsive">
+          <table class="invoice-list-table table">
+            <thead>
+              <tr>
+                <th></th>
+                <th>#</th>
+                <th><i data-feather="trending-up"></i></th>
+                <th>Client</th>
+                <th>Total</th>
+                <th class="text-truncate">Issued Date</th>
+                <th>Balance</th>
+                <th>Invoice Status</th>
+                <th class="cell-fit">Actions</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/ List DataTable -->
 </section>
 <!-- Dashboard Analytics end -->
 @endsection
@@ -256,15 +512,16 @@
   <script src="{{ asset('js/scripts/pages/dashboard-analytics.js') }}"></script>
   <script src="{{ asset('js/scripts/pages/app-invoice-list.js') }}"></script>
 
+
   <script>
-    		function copyReferralsLink(){   
-            let copyText = $('#referrals_link').attr('data-link');
-            const textArea = document.createElement('textarea');
-            textArea.textContent = copyText;
-            document.body.append(textArea);      
-            textArea.select();      
-            document.execCommand("copy");    
-            textArea.remove();
-        }
-  </script>
+    function copyReferralsLink(){   
+        let copyText = $('#referrals_link').attr('data-link');
+        const textArea = document.createElement('textarea');
+        textArea.textContent = copyText;
+        document.body.append(textArea);      
+        textArea.select();      
+        document.execCommand("copy");    
+        textArea.remove();
+    }
+</script>
 @endsection
